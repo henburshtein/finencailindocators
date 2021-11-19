fe7935b615cee9ca5012e8e26388148ee63661ed
henburshtein <85412921+henburshtein@users.noreply.github.com>
2021-11-19 21:55:53 +0200

    Add files via upload

---
+++ OBV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 04:37:44 2021
+
+@author: henburshtein
+"""
+
+import sys
+sys.path.insert(1, '/Users/henburshtein/Downloads/twsapi_macunix.976.01/IBJts/source/pythonclient/ibapi/tickerspull/fundamentalpull')
+import ta
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+from contractstock import stock as s
+import csv
+
+df = yf.Ticker(s["symbol"]).history(period="max").reset_index()[["Date" , "Close" , "High", "Low", "Volume"]]
+
+df['obv'] = ta.volume.OnBalanceVolumeIndicator(df['Close'], df['Volume'], fillna = False).on_balance_volume()
+df['obv'].to_csv("obv-aapl" + ".csv", sep=',')
+print("done")
+
+plt.plot(df["Date"],df["obv"])
+plt.show()
